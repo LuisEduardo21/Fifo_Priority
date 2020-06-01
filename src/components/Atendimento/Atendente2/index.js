d7743c770d375fc9fa4b019d4e75e118438357d2
dannsilva <danillo.dms@hotmail.com>
2020-05-31 23:37:13 -0300

    Criação de Atendimento, Espera e Finalizados

---
+++ src/components/Atendimento/Atendente2/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './styles.css';
+
+export function Atendente2() {
+  return <div className='containerAtendente2'>Atendente 2</div>;
+}
---
+++ src/components/Atendimento/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Atendente1 } from './Atendente1';
+import { Atendente2 } from './Atendente2';
+
+
+export function Atendimento() {
+  return (
+    <>
+      <Atendente1></Atendente1>
+      <Atendente2></Atendente2>
+    </>
+  );
+}
