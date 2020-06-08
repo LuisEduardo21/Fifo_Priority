86e69a9ff729a74ec7c7ee33862ce13cf9bd8b72
XxAllissonxX <allisson-f@hotmail.com>
2020-06-08 00:58:53 -0300

    adicionando funcionalidade aos botoes.

--- src/pages/Sistema/index.js
+++ src/pages/Sistema/index.js
@@ -15,6 +15,7 @@ export function Sistema() {
   const [filaAtendimento2, setFilaAtendimento2] = useState([]);
   const [filaFinalizada, setFilaFinalizada] = useState([]);
   const [tempoMedioEspera, setTempoMedioEspera] = useState([]);
+  const [executando, setExecutando] = useState(false);
 
   const qtdPessoasQueChegaramNaFila = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -32,104 +33,115 @@ export function Sistema() {
 
   //ele gerencia a fila de Espera
   useEffect(() => {
-    const interval = setInterval(() => {
-      //adiciona 1 segundo no tempo de fila de espera de cada cliente
-      if (filaDeEspera.length > 0) {
-        filaDeEspera.forEach((cliente) => cliente.tempoEspera++);
-        console.log(filaDeEspera);
-      }
-      if (filaDeEspera.length <= 9) {
-        const qtdClientes = qtdPessoasQueChegaramNaFila(0, 2);
-        for (let i = 0; i < qtdClientes; i++) {
+    if (executando) {
+      const interval = setInterval(() => {
+        //adiciona 1 segundo no tempo de fila de espera de cada cliente
+        if (filaDeEspera.length > 0) {
+          filaDeEspera.forEach((cliente) => cliente.tempoEspera++);
+        }
+        if (filaDeEspera.length <= 9) {
+          const qtdClientes = qtdPessoasQueChegaramNaFila(0, 2);
+          for (let i = 0; i < qtdClientes; i++) {
+            const cliente = geraCliente();
+            setFilaDeEspera((filaDeEspera) => [...filaDeEspera, cliente]);
+          }
+        } else if (filaDeEspera.length === 9) {
           const cliente = geraCliente();
           setFilaDeEspera((filaDeEspera) => [...filaDeEspera, cliente]);
         }
-      } else if (filaDeEspera.length === 9) {
-        const cliente = geraCliente();
-        setFilaDeEspera((filaDeEspera) => [...filaDeEspera, cliente]);
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [filaDeEspera]);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [filaDeEspera,executando]);
 
   //gerencia a primeira fila de atendimento
   useEffect(() => {
-    if (filaAtendimento1.length <= 0) {
-      const interval = setInterval(() => {
-        const cliente = filaDeEspera.shift();
-        setFilaDeEspera([...filaDeEspera]);
-        setFilaAtendimento1((filaAtendimento1) => [
-          ...filaAtendimento1,
-          cliente,
-        ]);
-      }, 2000);
-      return () => clearInterval(interval);
+    if (executando) {
+      if (filaAtendimento1.length <= 0) {
+        const interval = setInterval(() => {
+          const cliente = filaDeEspera.shift();
+          setFilaDeEspera([...filaDeEspera]);
+          setFilaAtendimento1((filaAtendimento1) => [
+            ...filaAtendimento1,
+            cliente,
+          ]);
+        }, 2000);
+        return () => clearInterval(interval);
+      }
     }
-  }, [filaDeEspera, filaAtendimento1]);
+  }, [filaDeEspera, filaAtendimento1,executando]);
 
   //Gerencia a segunda fila de atendimento
   useEffect(() => {
-    //Verificar se há cliente prioritario na fila
-    if (filaAtendimento2.length <= 0) {
-      const interval = setInterval(() => {
-        filaDeEspera.sort(function (a, b) {
-          return a.prioridade === b.prioridade ? 0 : a.prioridade ? -1 : 1;
-        });
-        const cliente = filaDeEspera.shift();
-        setFilaDeEspera([...filaDeEspera]);
-        setFilaAtendimento2((filaAtendimento2) => [
-          ...filaAtendimento2,
-          cliente,
-        ]);
-      }, 2000);
-      return () => clearInterval(interval);
+    if (executando) {
+      //Verificar se há cliente prioritario na fila
+      if (filaAtendimento2.length <= 0) {
+        const interval = setInterval(() => {
+          filaDeEspera.sort(function (a, b) {
+            return a.prioridade === b.prioridade ? 0 : a.prioridade ? -1 : 1;
+          });
+          const cliente = filaDeEspera.shift();
+          setFilaDeEspera([...filaDeEspera]);
+          setFilaAtendimento2((filaAtendimento2) => [
+            ...filaAtendimento2,
+            cliente,
+          ]);
+        }, 2000);
+        return () => clearInterval(interval);
+      }
     }
-  }, [filaDeEspera, filaAtendimento2]);
+  }, [filaDeEspera, filaAtendimento2,executando]);
 
   // Remove os clientes do atendimento 1
   useEffect(() => {
-    const interval = setInterval(() => {
-      const clienteAtendido = filaAtendimento1.shift();
-      setFilaAtendimento1([...filaAtendimento1]);
-      setFilaFinalizada((filaAtendimento1) => [
-        ...filaAtendimento1,
-        clienteAtendido,
-      ]);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [filaFinalizada, filaAtendimento1]);
+    if (executando) {
+      const interval = setInterval(() => {
+        const clienteAtendido = filaAtendimento1.shift();
+        setFilaAtendimento1([...filaAtendimento1]);
+        setFilaFinalizada((filaAtendimento1) => [
+          ...filaAtendimento1,
+          clienteAtendido,
+        ]);
+      }, 3000);
+      return () => clearInterval(interval);
+    }
+  }, [filaFinalizada, filaAtendimento1,executando]);
 
   // Remove os clientes do atendimento 2
   useEffect(() => {
-    const interval = setInterval(() => {
-      const clienteAtendido2 = filaAtendimento2.shift();
-      setFilaAtendimento2([...filaAtendimento2]);
-      setFilaFinalizada((filaAtendimento2) => [
-        ...filaAtendimento2,
-        clienteAtendido2,
-      ]);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [filaFinalizada, filaAtendimento2]);
+    if (executando) {
+      const interval = setInterval(() => {
+        const clienteAtendido2 = filaAtendimento2.shift();
+        setFilaAtendimento2([...filaAtendimento2]);
+        setFilaFinalizada((filaAtendimento2) => [
+          ...filaAtendimento2,
+          clienteAtendido2,
+        ]);
+      }, 3000);
+      return () => clearInterval(interval);
+    }
+  }, [filaFinalizada, filaAtendimento2,executando]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      //Tempo medio de fila de espera
-      if (filaFinalizada.length > 0) {
-        let contadorTempoMedio = 0;
-        filaFinalizada.forEach(
-          (cliente) =>
-            (contadorTempoMedio +=
-              cliente !== undefined ? cliente.tempoEspera : 0)
-        );
-        const tempoMedio = (contadorTempoMedio / filaFinalizada.length).toFixed(
-          2
-        );
-        setTempoMedioEspera(tempoMedio);
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  });
+    if (executando) {
+      const interval = setInterval(() => {
+        //Tempo medio de fila de espera
+        if (filaFinalizada.length > 0) {
+          let contadorTempoMedio = 0;
+          filaFinalizada.forEach(
+            (cliente) =>
+              (contadorTempoMedio +=
+                cliente !== undefined ? cliente.tempoEspera : 0)
+          );
+          const tempoMedio = (
+            contadorTempoMedio / filaFinalizada.length
+          ).toFixed(2);
+          setTempoMedioEspera(tempoMedio);
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  },[filaFinalizada,executando]);
 
   return (
     <div className="container-fluid">
@@ -142,6 +154,7 @@ export function Sistema() {
             type="button"
             class="btn btn-md btn-success"
             style={{ marginRight: 25 }}
+            onClick={()=>setExecutando(!executando)}
           >
             Iniciar
           </button>
@@ -149,12 +162,11 @@ export function Sistema() {
             type="button"
             class="btn btn-md btn-danger"
             style={{ marginRight: 25 }}
+            onClick={()=>setExecutando(!executando)}
           >
             Pausar
           </button>
-          <button type="button" class="btn btn-md btn-primary">
-            Reiniciar
-          </button>
+          
         </div>
         {/* fila de atendimento1*/}
         <div className="row  col-lg-12">
